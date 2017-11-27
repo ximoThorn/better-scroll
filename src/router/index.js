1ba25ba9365a040861f9ed94a9aa528bbe7f8475
ximoThorn <ximoThorn@163.com>
2017-11-27 09:44:45 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,24 @@ import swiper from '@/components/swiper'
 import selectOptions from '@/components/selectOptions'
 import indexList from "@/components/indexList"
 import dataPolymerization from "@/components/dataPolymerization"
+import polymerizationChild from "@/components/polymerizationChild"
+
+
+
+//vue组件路由懒加载写法
+// const index = () => import('@/components/index');
+// const scrolllist = () => import('@/components/scroll_list');
+// const pullDownOrUp = () => import('@/components/pullDownOrUp');
+// const swiper = () => import('@/components/swiper');
+// const selectOptions = () => import('@/components/selectOptions');
+// const indexList = () => import('@/components/indexList');
+// const dataPolymerization = () => import('@/components/dataPolymerization');
+//const polymerizationChild = () => import('@/components/polymerizationChild');
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history',//路由history模式
   routes: [
     {
       path: '/',
@@ -42,7 +56,20 @@ export default new Router({
     },
     {
       path: "/dataPolymerization",
-      component: dataPolymerization
+      component: dataPolymerization,
+      children: [
+        {
+          path: "/dataPolymerization/polyChild/:id",
+          name: "polyChild",
+          component: polymerizationChild,
+          beforeEnter: (to, from, next) => {//路由独享的守卫
+            console.log(to);
+            console.log(from);
+            console.log(to.params)
+            next();
+          }
+        }
+      ]
     }
   ]
 })
